cc1106e2b21275698046422b4c4099608eeafc7d
tarik <tarikboka123@gmail.com>
2024-11-29 16:15:47 +0100

    update

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <string.h>
+#include <conio.h>
+#include <ctype.h>
+#include "FUNCTION.H"
+int main() {
+
+homepage();
+
+
+    return 0;
+}
+
+
